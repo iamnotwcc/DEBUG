50e40ab8f6c75e6c6d410acacc70c28b11df5368
lishuai <2436700441@qq.com>
2020-06-25 22:12:02 +0800

    Finished debugging 7

---
+++ 7/1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+	int year;
+	int mouth;
+	int day ;
+	scanf("%d-%d-%d", &year, &mouth, &day);
+	if (mouth <= 12 && mouth >= 1) {
+		if (mouth ==1 || mouth == 3 || mouth == 5 || mouth == 7 || mouth== 8 || mouth == 10 || mouth == 12) {
+			if (day <= 31 && day >= 1) {
+				printf("YES");
+			} else {
+				printf("NO");
+			}
+		} else if (mouth == 4 || mouth == 6 || mouth == 9|| mouth == 11) {
+			if (day <= 30 && day >= 1) {
+				printf("YES");
+			} else {
+				printf("NO");
+			}
+		} else if (mouth == 2) {
+			if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+				if (day >= 1 && day <=29) {
+					printf("YES");
+				} else {
+					printf("NO");
+				}
+			} else {
+				if (day >= 1 && day <= 28) {
+					printf("YES");
+				} else {
+					printf("NO");
+				}
+			}
+        } else {
+			printf("NO");
+		}
+    } else {
+		printf("NO");
+	}
+	printf("\n");
+
+	return 0;
+}
+    
---
+++ 7/3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d-%d-%d", &a, &b, &c);
+    if(b>12||c>31) {
+		printf("NO");
+	} else if (b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12) {
+        if (c <= 31) {
+			printf("YES");
+		} else {
+			printf("NO");
+		}
+    } else if (b != 2) {
+        if (c > 30) {
+			printf("NO");
+		} else {
+			printf("YES");
+		}
+    } else if ((a % 100 != 0 && a % 4 == 0) || a % 400 == 0) {
+        if (c >= 1 && c <= 29) {
+			printf("YES");
+		} else {
+			printf("NO");
+		}
+    } else {
+		if (c >= 1 && c <= 28) {
+			printf("YES");
+		} else {
+			printf("NO");
+		}
+    }
+	printf("\n");
+
+    return 0;
+}

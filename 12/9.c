401385eed2dde42dbbaba9ae7fbf6fe06ceb5a1f
lishuai <2436700441@qq.com>
2020-06-26 15:53:34 +0800

    Finished debugging 12

--- 12/9.c
+++ 12/9.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 #define PI 3.14159
+
 int main() {
-	float r;
-    float c, s;
-	scanf("%g", &r);
+	double r;
+    double c, s;
+	scanf("%lf", &r);
     c = 2 * PI * r;
     s = PI * r *r;
-    printf("%.3f\n",c);
-    printf("%.3f\n",s);
-    return 0;
+    printf("%.3f\n", c);
+    printf("%.3f\n", s);
+    
+	return 0;
 }

656a13d3f8c056955d10a47cdebc3bdd9b689624
iamnotwcc <1902179510@qq.com>
2020-06-27 16:02:57 +0800

    debug-2

---
+++ 2/4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int matrix_a[10][10];
+    int matrix_b[10][10];
+    int m;
+    int n;
+    int i;
+    int j;
+    int k;
+    int m3[10][10] = {0};
+    
+    scanf("%d %d", &m, &n);
+    for(i = 0; i < m ; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix_a[i][j]);
+        }
+    }
+    
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            scanf("%d", &matrix_b[i][j]);
+        }
+    }
+    
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < m; j++) {
+            printf("%d ", m3[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < m; j++) {
+            for(k = 0; k < n; k++) {
+                m3[i][j] += matrix_a[i][k] * matrix_b[k][j];
+            }
+        }
+    }
+    
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < m; j++) {
+            printf("%d", m3[i][j]);
+            if(j != m - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

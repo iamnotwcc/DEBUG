8e6f22151dd3795e9d2eddb5bad6c9ca54b9d288
iamnotwcc <1902179510@qq.com>
2020-06-28 13:19:00 +0800

    debug-10

--- 10/1.c
+++ 10/1.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-int main() {
-    int a,b,c,d;
-    scanf("%d",&a);
-    for(d=1;d<=a;d+=1){
-        for(b=1;b<d;b+=1){
-            c=0;
-            if(d%b==0){
-             c+=1;
+
+int main()
+{
+    int a, b, c, d;
+
+    scanf("%d", &a);
+
+    for(d = 2; d <= a; d++) {
+        for(b = 2; b < d; b++) {
+            c = 0;
+            if(d % b == 0) {
+                c++;
+                break;
             }
         }
-        if(c==0){
-            printf("%d\n",d);
+        if(c == 0) {
+            printf("%d\n", d);
         }
     }
-   
-        
+
     return 0;
 }
--- 10/4.c
+++ 10/4.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-int main() {
-int num;
-    scanf("%d",num);
+
+int main() 
+{
+    int num;
+    scanf("%d", &num);
     int digit;
     int a;
-    for(digit=2;digit<=num;digit++)
-    {
-        for(a=2;digit<num;a++) {
-            if(digit % a ==0) {
+
+    printf("2\n");
+
+    for(digit = 3; digit <= num; digit++) {
+        for(a = 2; a < num; a++) {
+            if(digit % a == 0) {
                 break;
             }
-        
+            if(a == digit - 1) {
+                printf("%d\n", digit);
             }
         }
-    if(a==digit) {
-        printf("digit/n");
     }
+
     return 0;
 }

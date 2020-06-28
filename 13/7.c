e79392c2dea0b52675478efd2f9a7a99d6b71660
iamnotwcc <1902179510@qq.com>
2020-06-28 16:02:19 +0800

    debug-13

--- 13/7.c
+++ 13/7.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct node {
     int data;
     struct node *next;
@@ -8,7 +9,8 @@ typedef struct node {
 Node *circle_create(int n);
 void count_off(Node *head, int n, int k, int m);
 
-int main() {
+int main() 
+{
     int n, k, m;
     scanf("%d%d%d", &n, &k, &m);
     Node *head = circle_create(n);
@@ -20,12 +22,10 @@ Node *circle_create(int n) {
     Node *temp, *new_node, *head;
     int i;
 
-    // 创建第一个链表节点并加数据
     temp = (Node *) malloc(sizeof(Node));
     head = temp;
     head->data = 1;
 
-    // 创建第 2 到第 n 个链表节点并加数据
     for(i = 2; i <= n; i++) {
         new_node = (Node *) malloc(sizeof(Node));
         new_node->data = i;
@@ -33,33 +33,34 @@ Node *circle_create(int n) {
         temp = new_node;
     }
 
-    // 最后一个节点指向头部构成循环链表
     temp->next = head;
 
     return head;
 }
 
-void count_off(Node *head, int n, int k, int m) {
-    Node *p=head;
+void count_off(Node *head, int n, int k, int m) 
+{
+    Node *p = head;
     Node *q;
-    if(p!=NULL){
-    	while(p->data!=k){
-        	p=p->next;
+    if(p != NULL) {
+    	while(p->data != k) {
+        	p = p->next;
         }
-        while(p!=p->next){
-            int j=1;
-            while(j<m-1){
-            	p=p->next;
+        while(p != p->next) {
+            int j = 1;
+            while(j < m - 1) {
+            	p = p->next;
             	++j;
 			}
-            q=p->next;
-            printf("%d ",q->data);
-            p->next=q->next;
+            q = p->next;
+            printf("%d ", q->data);
+            p->next = q->next;
             free(q);
-            q=NULL;
-            p=p->next;
+            q = NULL;
+            p = p->next;
         }
-        printf("%d",p->data);
+        printf("%d", p->data);
     }
+
     return;
 }

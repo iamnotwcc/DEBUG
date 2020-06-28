d36c759311846b4731247cdfc784f870d2002168
iamnotwcc <1902179510@qq.com>
2020-06-28 16:27:44 +0800

    debug-14

--- 14/4.c
+++ 14/4.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-int main() {
-	int n;
+
+int main() 
+{
+    int n;
+
     scanf("%d", &n);
     
     for (int i = n; i > 0; i--) {
         for (int j = i; j > 0; j--) {
-            printf("%d", j);
+            if (j > 1) {
+                printf("%d ", j);
+            } else {
+                printf("%d", j);
+            }
         }
-        
         printf("\n");
     }
 

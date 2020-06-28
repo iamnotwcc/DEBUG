573ba3f1bff93aba722d6d5f714e4fd72d3a7437
iamnotwcc <1902179510@qq.com>
2020-06-28 12:33:10 +0800

    debug-9

--- 9/10.c
+++ 9/10.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-int f(int n){
-    if(n<=3){return 1;}
-    else{return f(n-2)+f(n-3);}
+
+int f(int n)
+{
+    if(n <= 4) {
+        return 1;
+    } else {
+        return f(n-2) + f(n-3);
+    }
 }
-int main() {
+
+int main() 
+{
     int n;
+
     scanf("%d", &n);
     printf("%d", f(n));
+
     return 0;
 }
--- 9/7.c
+++ 9/7.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int i,N,x;
+int main() 
+{
+    int i, N, x;
     int a[51] = {0};
-    scanf("%d",&N);
-    a[2] = 2;
-    a[3] = 3;
-    for(i = 3;i <= N;i++){
+
+    a[2] = 1;
+    a[3] = 1;
+    a[4] = 1;
+
+    scanf("%d", &N);
+
+    for(i = 5; i <= N; i++) {
         a[i] = a[i - 2] + a[i - 3];
     }
-    printf("%d",a[N]);
+
+    printf("%d", a[N]);
+
     return 0;
 }

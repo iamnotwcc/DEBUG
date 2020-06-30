eebc1ecb08ef829b087338c9e59f7fad5f05878d
LittleSquirrel777 <65956712+LittleSquirrel777@users.noreply.github.com>
2020-06-30 14:35:18 +0800

    Update 5.c
    Finished

--- 11/5.c
+++ 11/5.c
@@ -4,7 +4,7 @@
 int main()
 {
     char input[500];
-    char arr[32] = {0};
+    int arr[32] = {0};
     int bits[32];
     char result[33];
     scanf("%s", input);
@@ -13,7 +13,7 @@ int main()
     int length = strlen(input);
     
     for (int i = 0; i < length; i++) {
-        arr[(i + 1) % 32] = arr[(i+1) % 32] + (int)(input[i]);
+        arr[(i + 1) % 32] = arr[(i + 1) % 32] + (int)(input[i]);
     }
     
     for (int j = 0; j < 32; j++) {

512fad62dfdb553262f7d90f2620932b052c3a21
LittleSquirrel777 <65956712+LittleSquirrel777@users.noreply.github.com>
2020-06-30 13:51:47 +0800

    Update 2.c
    Finished

--- 7/2.c
+++ 7/2.c
@@ -6,7 +6,7 @@ int main()
     int month;
     int day;
     scanf("%d-%d-%d", &year, &month, &day);
-    if ((month == 1 && month == 3 && month == 5 && month == 7 && month == 8 && month == 10 && month == 12) && day >= 1 && day <= 31) {
+    if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day >= 1 && day <= 31) {
         printf("YES");
     } else if ((month == 4 && month == 6 && month == 9 && month == 11) && day >= 1 && day <= 30) {
         printf("YES");

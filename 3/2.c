97232d757440910f41721301d16fbedf67667951
LittleSquirrel777 <65956712+LittleSquirrel777@users.noreply.github.com>
2020-06-30 23:22:43 +0800

    Finished

--- 3/2.c
+++ 3/2.c
@@ -18,66 +18,66 @@ int main()
     if (m == 1 || n == 1) {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-    			printf("%d", matrix[i][j]);
+    		    printf("%d", matrix[i][j]);
             	if (j != n - 1) {
-    			    printf(" ");
+                    printf(" ");
     			}
             }
             if (i != m - 1) {
-    		    printf(" ");
+                printf(" ");
     		}
         }
     } else {
     	while (i != k) {
     		for (int j = n - n0; j < n0; j++) {
-    		    printf("%d", matrix[m - m0][j]);
+                printf("%d", matrix[m - m0][j]);
     		    i++;
     		    if (i == k) {
-    		            break;
+                    break;
     			} else {
-    		        printf(" ");
+                    printf(" ");
     			}
     		}
     		if (i == k) {
-    		    break;
+                break;
     		}
     		for (int j = m - m0 + 1; j < m0; j++) {
-    		    printf("%d", matrix[j][n0 - 1]);
+                printf("%d", matrix[j][n0 - 1]);
     		    i++;
     			if(i == k) {
-    		            break;
+                    break;
     			} else {
-    		        printf(" ");
+                    printf(" ");
     			}
     		}
     		if (i == k) {
-    		    break;
+                break;
     		}
     		n0--;
     		for (int j = n0 - 1; j >= n - n0 - 1; j--) {
-    		    printf("%d", matrix[m0 - 1][j]);
+                printf("%d", matrix[m0 - 1][j]);
     		    i++;
     			if(i == k) {
-    			    break;
+                    break;
     			} else {
-    		        printf(" ");
+                    printf(" ");
     			}
     		}
     		if (i == k) {
-    		    break;
+                break;
     		}
     		m0--;
     		for (int j = m0 - 1; j >= m - m0; j--) {
     		    printf("%d", matrix[j][n - n0 - 1]);
     			i++;
     			if(i == k) {
-    			        break;
+                    break;
     			} else {
-    			    printf(" ");
+                    printf(" ");
     			}
     		}
     		if (i == k) {
-    		    break;
+                break;
     		}
     	}
     }

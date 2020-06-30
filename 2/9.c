9117362e90972cd2ebc6927281689f1133ffda4b
LittleSquirrel777 <65956712+LittleSquirrel777@users.noreply.github.com>
2020-06-30 12:48:13 +0800

    Update 9.c
    Finished

--- 2/9.c
+++ 2/9.c
@@ -7,34 +7,23 @@ int main()
     int matrix_c[10][10];
     int m;
     int n;
-    
-    //主循环参数
     int i, j, k;
-	
-    //乘积矩阵初始化
     for (i = 0; i < 10; i++){
 	for (k = 0; k < 10; k++){
 	    matrix_c[i][k] = 0;
 	}
     }
-	
-    //m、n输入
     scanf("%d %d", &m, &n);
-	
-    //矩阵输入
     for (i = 0; i < m; i++){
 	for (k = 0; k < n; k++){
 	    scanf("%d", &matrix_a[i][k]);
 	}	
-    }
-		
+    }	
     for (k = 0; k < n; k++){
         for (i = 0; i < m; i++){
 	    scanf("%d", &matrix_b[k][i]);
 	}
     }
-	
-    //乘法
     for (i = 0; i < m; i++) {
 	for (j = 0; j < m; j++) {
 	    for (k = 0; k < n; k++) {

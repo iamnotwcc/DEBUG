5759790a7f8fb9ae4b7933cdfc2bf017cc5c0367
LittleSquirrel777 <65956712+LittleSquirrel777@users.noreply.github.com>
2020-06-30 13:56:42 +0800

    Update 7.c
    Finished

--- 8/7.c
+++ 8/7.c
@@ -8,15 +8,13 @@ int main()
     scanf("%d", &n);
     char names[n][101];
     char temp[101];
-    while (scanf("%c", &name[i][j] != EOF)) {
-    	for (i = 0; i < n; i++) {
-    		for (j = 0; j < 101; j++) {
-    			scanf("%c", &names[i][j]);
-    			if (names[i][j] == '\n') {
-    				names[i][j] = '\0';
-    				break;
-    			}            
-    		}
+    for (i = 0; i < n; i++) {
+    	for (j = 0; j < 101; j++) {
+    		scanf("%c", &names[i][j]);
+    		if (names[i][j] == '\n') {
+    			names[i][j] = '\0';
+    			break;
+    		}            
     	}
     }
     

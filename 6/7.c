3d973f6e334d23133e0022d148e0d5d95d786cf5
LittleSquirrel777 <65956712+LittleSquirrel777@users.noreply.github.com>
2020-06-30 13:45:02 +0800

    Update 7.c
    Finished

--- 6/7.c
+++ 6/7.c
@@ -14,18 +14,20 @@ int main()
         rwx ^= (1 << 1);
         scanf("%c", &temp);
         if (temp == 'x') {
-    		rwx ^= (1 << 0);
+    	    rwx ^= (1 << 0);
     	}
     } else if (temp == 'r') {
         rwx ^= (1 << 2);
         scanf("%c", &temp);
         if (temp == 'w') {
-    		rwx ^= (1 << 1);
-    	}
-        scanf("%c", &temp);
-        if (temp == 'x') {
-    		rwx ^= (1 << 0);
-    	}
+    	    rwx ^= (1 << 1);
+            scanf("%c", &temp);
+            if (temp == 'x') {
+    	        rwx ^= (1 << 0);
+    	    }
+    	} else if (temp == 'x'){
+            rwx ^= (1 << 0);
+        }
     }
     while (scanf("%c", &temp) != EOF) {
         if (temp == '+') {
@@ -43,21 +45,21 @@ int main()
 }
 void add(char input)
 {
-    if (input == 'r' && rwx / 4 != 1) {
+    if (input == 'r' && (rwx / 4 != 1)) {
     	rwx ^= (1 << 2);
     } else if (input == 'w' && (rwx / 2 % 2 != 1)) {
     	rwx ^= (1 << 1);
-    } else if (rwx % 4 != 1) {
+    } else if (input == 'x' && (rwx % 2 != 1)) {
     	rwx ^= (1 << 0);
     }
 }
 void delete1(char input)
 {
-    if (input == 'r' && rwx / 4 == 1) {
+    if (input == 'r' && (rwx / 4 == 1)) {
     	rwx ^= (1 << 2);
     } else if (input == 'w' && (rwx / 2 % 2 == 1)) {
     	rwx ^= (1 << 1);
-    } else if (rwx % 4 == 1) {
+    } else if (input == 'x' && (rwx % 2 == 1)) {
     	rwx ^= (1 << 0);
     }
 }
